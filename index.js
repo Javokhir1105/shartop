d8bbaf6e05a38abc35df772a2557844fb63b7ff1
Javokhir1105 <161455530+Javokhir1105@users.noreply.github.com>
2024-04-04 18:26:57 +0500

    shart

---
+++ index.js
@@ -0,0 +1,127 @@
+// 1
+// let x=prompt("sonni kirting")
+// if(x%2==0){
+//     console.log(x*x)
+// }
+// else if(x%2==1){
+//     console.log(Math.sqrt(x))
+// }
+// else{
+//     console.log("siz 0 sonini kiritdingiz")
+// }
+
+// 2
+// let x=prompt("matnni kiriting")
+// if(x.length>10){
+//     console.log(x.toLowerCase())
+// }
+// else{
+//     console.log(x.toUpperCase())
+// }
+
+// 3
+// let x=prompt("matnni kiriting")
+// if(x.length>15){
+//     console.log(x.repeat(2))
+// }
+// else{
+//     console.log(x)
+// }
+
+// 4
+// let x=prompt("matnni kiriting")
+// if(x.length>5){
+//     console.log(x.repeat(3))
+// }
+// else{
+//     console.log(x)
+// }
+
+
+// let x=prompt("matnni kiriting")
+// // let c=x.indexOf("a")
+// console.log(x.replace("a", '@'))
+
+// 6
+// let x=prompt("matnni kiriting")
+// if(x.length>10){
+//     console.log(x.slice(5))
+// }
+// else{
+//     console.log("bu so'z 10ta harfdan kam")
+// }
+
+// 7
+// let x=prompt("sonni kiriting")
+// console.log(x.toString())
+
+// 8
+// let x=prompt("matnni kiriting")
+// console.log(x.trim())
+
+// 9
+// let x=prompt("sonni kiriting")
+// if(x<0){
+//     console.log(Math.sqrt(x*x))
+// }
+// else{
+//     console.log("musbat son")
+// }
+
+// 10
+// let x=prompt("matnni kiriting")
+// if(x.length>20){
+//     console.log(x.slice(3))
+// }
+// else{
+//     console.log("bu so'z 20ta harfdan kam")
+// }
+
+// 11
+// let x=prompt("matnni kiriting")
+// console.log(x.replace("a",'A'))
+// console.log(x.replace("b",'B'))
+
+// 12
+// let x="java"
+// console.log(x.replace("java",'JavaScript'))
+
+// 13
+// let x=prompt("matnni kiriting")
+// let y="Hello TATU"
+// if(x.length>20){
+//     console.log(x.slice(10))
+// }
+// else{
+//     console.log(x.concat(" ", y))
+// }
+
+// 14
+// let x=prompt("sonni kiriting")-0
+// if(x<0){
+//     console.log(x*3)
+// }
+// else{
+//     console.log(x+10)
+// }
+
+// 15
+// let a=prompt("Son kiriting: ")-0
+// let b=prompt("Son kiriting: ")-0
+// let c=prompt("Ishirani kiriting")
+// if(c = "plus"){
+//     console.log(a + b);
+// }
+// else if(c = "ko'pay"){
+//     console.log(a * b);
+// }
+// else if(c = "minus"){
+//     console.log(a - b);
+// }
+// else if(c = "bo'lish"){
+//     console.log(a / b);
+// }
+// else{
+//     console.log("bunday belgi yuq")
+// }
+
